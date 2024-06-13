b7616b3d316590e351fe5d379dca94ac4bfe1be3
Aneri Patel <aneripatel2002@gmail.com>
2024-06-13 19:29:26 -0400

    added tables.sql and revised schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,3 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
-USE company_db;
 
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS job;
-DROP TABLE IF EXISTS employee;
-
-CREATE TABLE department (
-    id SERIAL PRIMARY KEY,
-    dept_name VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE job (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(5,3),
-    department_id INTEGER,
-    FOREIGN KEY (department_id) REFERENCES department (id)  
-);
-
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    job_id INTEGER,
-    FOREIGN KEY (job_id) REFERENCES job (id),
-    manager_id INTEGER,
-    FOREIGN KEY (manager_id) REFERENCES employee (id)
-);
