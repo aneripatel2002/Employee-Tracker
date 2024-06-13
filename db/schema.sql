dc0337d5c2a1ae9476e77e8ac6e39274db160a01
Aneri Patel <aneripatel2002@gmail.com>
2024-06-13 12:48:59 -0400

    corrections to department and ID

--- db/schema.sql
+++ db/schema.sql
@@ -7,21 +7,24 @@ DROP TABLE IF EXISTS job;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE job (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
+    salary DECIMAL(5,3),
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES department (id)  
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    job_id INT,
-    manager_id INT,
+    job_id INTEGER,
+    FOREIGN KEY (job_id) REFERENCES job (id),
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
